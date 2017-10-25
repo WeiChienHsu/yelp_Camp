07da69e4114edddc9fbf6116f92dc58b7ccf4967
WeiChien Hsu <weichien711@gmail.com>
2017-10-25 17:59:42 +0000

    add README

---
+++ README.md
@@ -0,0 +1,13 @@
+#YelpCamp APP
+
+##Initial Setup
+** Add Langing Page
+** Add Campground Page that lists all campgrounds
+
+Each Campground has:
+  *Name
+  *Image
+
+##Layout and Basic Styling
+**Create header and footer
+**Add in Bootstrap3
--- app.js
+++ app.js
@@ -8,22 +8,29 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req, res){
     res.render("landing");
 })
-
-app.get("/campgrounds",function(req, res){
+///
    var campgrounds = [
        {name:"YinMing Mountain", image:"https://photosforclass.com/download/5641024448"},
        {name:"Creep Mountain", image:"https://photosforclass.com/download/14435096036"},
        {name:"Camping Paradas", image:"https://photosforclass.com/download/15989950903"}
        ];
+///
+
+app.get("/campgrounds",function(req, res){
+
     res.render("campgrounds",{campgrounds:campgrounds});
 });
 
 // Fellow the RESTful to have a same path name
 app.post("/campgrounds",function(req, res){
-    res.send("you hit the post");
     //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name,image:image};
+    campgrounds.push(newCampground);
     
     // redirect back to campgrounds page
+    res.redirect("/campgrounds");
 });
 
 // give a form to send the data to POST ROUTE||||||
