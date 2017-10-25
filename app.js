9ae88c723e5301ce31a6b2b4156841761ae9227d
WeiChien Hsu <weichien711@gmail.com>
2017-10-25 16:12:57 +0000

    add campgrounds page

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get("/",function(req, res){
     res.render("landing");
 })
 
+app.get("/campgrounds",function(req, res){
+   var campgrounds = [
+       {name:"YinMing Mountain", image:"http://photosforclass.com/download/1342367857"},
+       {name:"Creep Mountain", image:"http://photosforclass.com/download/15989950903"},
+       {name:"Camping Paradas", image:"http://photosforclass.com/download/5641024448"}
+       ];
+    res.render("campgrounds",{campgrounds:campgrounds});
+});
+
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("The Yelp Camp server is connecting");
-})
+});
