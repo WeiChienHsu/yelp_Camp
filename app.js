f5d2e1a7752088de762591d05a66e148bcca85d9
WeiChien Hsu <weichien711@gmail.com>
2017-10-25 17:19:03 +0000

    add create page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function(req, res){
     res.render("landing");
@@ -9,12 +11,26 @@ app.get("/",function(req, res){
 
 app.get("/campgrounds",function(req, res){
    var campgrounds = [
-       {name:"YinMing Mountain", image:"http://photosforclass.com/download/1342367857"},
-       {name:"Creep Mountain", image:"http://photosforclass.com/download/15989950903"},
-       {name:"Camping Paradas", image:"http://photosforclass.com/download/5641024448"}
+       {name:"YinMing Mountain", image:"https://photosforclass.com/download/5641024448"},
+       {name:"Creep Mountain", image:"https://photosforclass.com/download/14435096036"},
+       {name:"Camping Paradas", image:"https://photosforclass.com/download/15989950903"}
        ];
     res.render("campgrounds",{campgrounds:campgrounds});
 });
+
+// Fellow the RESTful to have a same path name
+app.post("/campgrounds",function(req, res){
+    res.send("you hit the post");
+    //get data from form and add to campgrounds array
+    
+    // redirect back to campgrounds page
+});
+
+// give a form to send the data to POST ROUTE||||||
+
+app.get("/campgrounds/new",function(req, res){
+    res.render("new.ejs");
+})
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("The Yelp Camp server is connecting");
